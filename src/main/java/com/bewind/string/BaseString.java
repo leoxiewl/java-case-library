2bdc64588963a842d36bd5855eb71865bff09764
Leo Xie <leoxiewl@163.com>
2024-05-09 23:19:54 +0800

    String

--- src/main/java/com/bewind/string/BaseString.java
+++ src/main/java/com/bewind/string/BaseString.java
@@ -8,27 +8,52 @@ public class BaseString {
 
         // 字符串比较
         compareString();
+
+        // 字符串比较忽略大小写
+        compareStringIgnoreCase();
+
+        testCharAt();
+
+        testTrim();
     }
 
     // ✅
-    public static void createBean01(){
+    public static void createBean01() {
         String s1 = "hello";
         String s2 = "hello";
         System.out.println(s1 == s2); // true
     }
 
     // 创建两次对象，一次在堆中，一次在常量池中
-    public static void createBean02(){
+    public static void createBean02() {
         String s1 = new String("hello");
         String s2 = new String("hello");
         System.out.println(s1 == s2); // false
     }
 
     // 字符串比较
-    public static void compareString(){
+    public static void compareString() {
         String s1 = new String("hello");
         String s2 = new String("hello");
         System.out.println(s1.equals(s2)); // true
+    }
+
+    // 比较不区分大小写
+    public static void compareStringIgnoreCase() {
+        String str1 = "abc";
+        String str2 = "ABC";
+        System.out.println("比较不区分大小写: " + str1.equalsIgnoreCase(str2));
+    }
+
+    public static void testCharAt() {
+        String str = "hello";
+        char c = str.charAt(0);
+        System.out.println("testCharAt: " + c); // h
+    }
+
+    public static void testTrim(){
+        String str = " d f ";
+        System.out.println("去除空格: " + str.trim());
     }
 
 }
